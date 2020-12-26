04af84cf026212b1420b04c641f27534441c40cc
Juan Pablo Pachar <jpablopachar1993@gmail.com>
2020-12-25 23:29:28 -0500

    Funcionalidad para editar una oferta

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  AlertController,
+  LoadingController,
+  NavController
+} from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Place } from '../../place';
+import { PlaceService } from '../../places.service';
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
   styleUrls: ['./edit-offer.page.scss'],
 })
-export class EditOfferPage implements OnInit {
+export class EditOfferPage implements OnInit, OnDestroy {
+  private placeSubs: Subscription;
+  public place: Place;
+  public placeId: string;
+  public form: FormGroup;
+  public isLoading = false;
 
-  constructor() { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly alertCtrl: AlertController,
+    private readonly navCtrl: NavController,
+    private readonly loadingCtrl: LoadingController,
+    private readonly placeService: PlaceService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return;
+      }
+      this.placeId = paramMap.get('placeId');
+      this.isLoading = true;
+      this.placeSubs = this.placeService
+        .getPlace(paramMap.get('placeId'))
+        .subscribe(
+          (place) => {
+            this.place = place;
+            this.form = new FormGroup({
+              title: new FormControl(this.place.title, {
+                updateOn: 'blur',
+                validators: [Validators.required],
+              }),
+              description: new FormControl(this.place.description, {
+                updateOn: 'blur',
+                validators: [Validators.required, Validators.maxLength(180)],
+              }),
+            });
+            this.isLoading = false;
+          },
+          (error) => {
+            this.alertCtrl
+              .create({
+                header: 'An error occurred!',
+                message: 'Place could not be fetched. Please try again later.',
+                buttons: [
+                  {
+                    text: 'Okay',
+                    handler: () => {
+                      this.router.navigate(['/places/tabs/offers']);
+                    },
+                  },
+                ],
+              })
+              .then((alertEl) => {
+                alertEl.present();
+              });
+          }
+        );
+    });
   }
 
+  public onUpdateOffer(): void {
+    if (!this.form.valid) {
+      return;
+    }
+
+    this.loadingCtrl
+      .create({
+        message: 'Updating place...',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.placeService
+          .updatePlace(
+            this.place.id,
+            this.form.value.title,
+            this.form.value.description
+          )
+          .subscribe(() => {
+            loadingEl.dismiss();
+            this.form.reset();
+            this.router.navigate(['/places/tabs/offers']);
+          });
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.placeSubs) {
+      this.placeSubs.unsubscribe();
+    }
+  }
 }
